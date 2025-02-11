62f9ac88c088e5b8db8fc4b7f8a34ca11d32f4c3
麒翔 <qixiang.mft@taobao.com>
2025-02-11 14:31:17 +0800

    update gcc

--- shell.nix
+++ shell.nix
@@ -1,4 +1,6 @@
 
+
+
 { pkgs ? import <nixpkgs> { }, isMac ? false }:
 let
 
@@ -24,16 +26,14 @@ let
         echo  running command : ${pkgs.cmake}/bin/cmake -DADDITIONAL_TOOLCHAIN_PATH="${ENV_TC_PATH}" -DTOOLCHAIN_PREFIX=${ENV_TC_PREFIX}  "${
           "$"
         }{@}"
-      ${pkgs.cmake}/bin/cmake -DADDITIONAL_TOOLCHAIN_PATH="${
-        "$"
-      }{ENV_TC_PATH}" -DTOOLCHAIN_PREFIX=${ENV_TC_PREFIX}  "${"$"}{@}"
+      ${pkgs.cmake}/bin/cmake -DADDITIONAL_TOOLCHAIN_PATH="${ENV_TC_PATH}" -DTOOLCHAIN_PREFIX=${ENV_TC_PREFIX}  "${"$"}{@}"
     '';
   };
 
 in pkgs.mkShell {
   packages = with pkgs;
     [ ccache cmakeWrapper python311Custom ]
-    ++ (if isMac then [ riscv64-embedded ] else [ ]);
+    ++ (if isMac then [ riscv ] else [ ]);
   nativeBuildInputs = with pkgs.buildPackages;
     [
       #FIXME use mbedtls in submodule
